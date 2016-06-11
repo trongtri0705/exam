5c4a994c0d49cbc1cc526c38a29a85ae52e8e3d4
Tri Nguyen Trong <Tri Nguyen Trong>
2016-06-11 17:37:38 +0700

    abc

---
+++ custom/application/Ext/Include/modules.ext.php
@@ -0,0 +1,20 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+ 
+ //WARNING: The contents of this file are auto-generated
+$beanList['C_Bill'] = 'C_Bill';
+$beanFiles['C_Bill'] = 'modules/C_Bill/C_Bill.php';
+$moduleList[] = 'C_Bill';
+$beanList['C_Category'] = 'C_Category';
+$beanFiles['C_Category'] = 'modules/C_Category/C_Category.php';
+$moduleList[] = 'C_Category';
+$beanList['C_Product'] = 'C_Product';
+$beanFiles['C_Product'] = 'modules/C_Product/C_Product.php';
+$moduleList[] = 'C_Product';
+$beanList['C_Provider'] = 'C_Provider';
+$beanFiles['C_Provider'] = 'modules/C_Provider/C_Provider.php';
+$moduleList[] = 'C_Provider';
+
+
+?>
